43352e08d091ea2511d68e7199959331e95eafaa
Tsjippy <ewaldha@hotmail.com>
2024-12-17 12:37:27 +0100

    skip:

--- php/update.php
+++ php/update.php
@@ -7,9 +7,13 @@ function pluginUpdate($oldVersion){
     require_once ABSPATH . 'wp-admin/includes/upgrade.php';
     require_once ABSPATH . 'wp-admin/install-helper.php';
 
+    SIM\printArray($oldVersion);
+
     $simForms = new SimForms();
 
     if($oldVersion < '8.1.1'){
         maybe_add_column($simForms->tableName, 'reminder_amount', "ALTER TABLE $simForms->tableName ADD COLUMN `reminder_amount` LONGTEXT");
+
+        SIM\printArray("Added column");
     }
 }
