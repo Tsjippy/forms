fbb67930330df73f8d7c1357b56ca4dc7cb89ec0
Tsjippy <ewaldha@hotmail.com>
2026-06-01 13:22:11 +0200

    fixed: added domain to __ function

--- php/main.php
+++ php/main.php
@@ -10,7 +10,7 @@ add_filter('display_post_states', __NAMESPACE__.'\postStates', 10, 2);
 function postStates( $states, $post ) {
     
     if ($post->ID == SETTINGS['forms-page'] ?? '') {
-        $states[] = __('Form selector page');
+        $states[] = __('Form selector page', 'tsjippy');
     }
 
     return $states;
