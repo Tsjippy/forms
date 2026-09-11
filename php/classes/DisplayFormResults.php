efd9cc110ced5247b1422d97b007ebed3096ebb7
Tsjippy <ewaldha@hotmail.com>
2026-09-11 19:43:04 +0200

    fixed: display bug

--- php/classes/DisplayFormResults.php
+++ php/classes/DisplayFormResults.php
@@ -578,7 +578,7 @@ class DisplayFormResults extends SubmitForm
         $baseQuery            .= "SELECT * FROM Submissions WHERE 1=1";
 
         if (!empty($finalWhere)) {
-            TSJIPPY\printArray($finalWhere, $where);
+            TSJIPPY\printArray([$finalWhere, $where]);
             $baseQuery .= " AND " . implode(' AND ', $finalWhere);
         }
 
